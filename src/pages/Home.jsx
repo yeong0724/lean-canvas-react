44c7a79ee14e01ec12395dd025bfa02e2e0fba1b
Jinyeong Kim <kid4211@gmail.com>
2025-06-16 23:51:35 +0900

    react-query 적용 및 캐시 처리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,61 +1,78 @@
-import { useEffect, useState } from 'react';
-import CanvasList from '../components/CanvasList';
-import SearchBar from '../components/SearchBar';
-import ViewToggle from '../components/ViewToggle';
-import { getCanvases, createCanvas, deleteCanvas } from '../api/canvas';
-import Loading from '../components/Loading';
-import Error from '../components/Error';
-import Button from '../components/Button';
-import useApiRequest from '../hooks/useApiRequest';
+import { useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { getCanvases, createCanvas, deleteCanvas } from '@api/canvas';
+import CanvasList from '@components/CanvasList';
+import SearchBar from '@components/SearchBar';
+import CategoryFilter from '@components/CategoryFilter';
+import ViewToggle from '@components/ViewToggle';
+import Loading from '@components/Loading';
+import Error from '@components/Error';
+import Button from '@components/Button';
 
 function Home() {
-  const [searchText, setSearchText] = useState('');
-  const [isGridView, setIsGridView] = useState(true);
-  const [data, setData] = useState([]);
+  const queryClient = useQueryClient();
 
-  // API call
-  const { isLoading, error, execute: fetchData } = useApiRequest(getCanvases);
-  const { isLoading: isLoadingCreate, execute: createNewCanvas } =
-    useApiRequest(createCanvas);
+  const [isGridView, setIsGridView] = useState(true);
+  const [filter, setFilter] = useState({
+    searchText: undefined,
+    category: undefined,
+  });
+
+  // 데이터 조회
+  const { data, isLoading, error, refetch } = useQuery({
+    queryKey: ['canvases', filter.searchText, filter.category],
+    queryFn: () => {
+      return getCanvases({
+        title_like: filter.searchText,
+        category: filter.category,
+      });
+    },
+    staleTime: 1000 * 60, // 1분 동안 데이터가 신선함(fresh) 상태로 유지 -> 새로고침을 해도 서버에 요청하지 않음
+    refetchOnWindowFocus: false, // 창이 포커스될 때마다 데이터를 새로 가져오지 않음 (ex. 탭이동)
+  });
+
+  // 등록
+  const { mutate: createNewCanvas, isLoading: isLoadingCreate } = useMutation({
+    mutationFn: createCanvas,
+    onSuccess: () => queryClient.invalidateQueries(['canvases']),
+    onError: err => alert(err.message),
+  });
+
+  // 삭제
+  const { mutate: deleteCanvasMutation } = useMutation({
+    mutationFn: deleteCanvas,
+    onSuccess: () => queryClient.invalidateQueries(['canvases']),
+    onError: err => alert(err.message),
+  });
 
   const handleDeleteItem = async id => {
-    if (!confirm('삭제 하시겠습니까?')) return;
-
-    try {
-      await deleteCanvas(id);
-      fetchData({ title_like: searchText });
-    } catch (err) {
-      alert(err.message);
-    }
+    deleteCanvasMutation(id);
   };
 
-  useEffect(() => {
-    fetchData(
-      { title_like: searchText },
-      {
-        onSuccess: response => setData(response.data),
-      },
-    );
-  }, [searchText, fetchData]);
-
   const handleCreateCanvas = async () => {
-    createNewCanvas(null, {
-      onSuccess: () => {
-        fetchData(
-          { title_like: searchText },
-          {
-            onSuccess: response => setData(response.data),
-          },
-        );
-      },
-      onError: err => alert(err.message),
+    createNewCanvas();
+  };
+
+  const handleFilter = (key, value) => {
+    setFilter({
+      ...filter,
+      [key]: value,
     });
   };
 
   return (
     <div>
       <div className="mb-6 flex flex-col sm:flex-row items-center justify-between">
-        <SearchBar searchText={searchText} setSearchText={setSearchText} />
+        <div className="flex gap-2 flex-col w-full sm:flex-row mb-4 sm:mb-0">
+          <SearchBar
+            searchText={filter.searchText}
+            onSearch={val => handleFilter('searchText', val)}
+          />
+          <CategoryFilter
+            category={filter.category}
+            onChange={val => handleFilter('category', val)}
+          />
+        </div>
         <ViewToggle isGridView={isGridView} setIsGridView={setIsGridView} />
       </div>
       <div className="border-t-2 border-gray-200 mb-5" />
@@ -65,18 +82,12 @@ function Home() {
         </Button>
       </div>
       {isLoading && <Loading />}
-      {error && (
-        <Error
-          message={error.message}
-          onRetry={() => fetchData({ title_like: searchText })}
-        />
-      )}
-
+      {error && <Error message={error.message} onRetry={refetch} />}
       {!isLoading && !error && (
         <CanvasList
           filteredData={data}
           isGridView={isGridView}
-          searchText={searchText}
+          searchText={filter.searchText}
           onDeleteItem={handleDeleteItem}
         />
       )}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@api': '/src/api',
+      '@components': '/src/components',
+      '@hooks': '/src/hooks',
+      '@pages': '/src/pages',
+      '@styles': '/src/styles',
+    },
+  },
+});
